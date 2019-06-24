d44f4fb61add4b04ecf3867193e8470e10b197da
Matt <mattmleach@gmail.com>
2019-06-24 15:29:34 -0400

    port update

--- server.js
+++ server.js
@@ -40,6 +40,6 @@ app.get("/api/hello", function (req, res) {
 // });
 
 // listen for requests :)
-var listener = app.listen(process.env.port, function () {
+var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
